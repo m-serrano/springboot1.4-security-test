1c250939f09981ab08f5a15ad978dd035fe4e91b
mserrano <mserrano@consisint.com>
2019-11-02 16:15:10 -0500

    import

--- src/main/java/com/example/mserrano/security/test/config/ResourceServerConfiguration.java
+++ src/main/java/com/example/mserrano/security/test/config/ResourceServerConfiguration.java
@@ -3,6 +3,7 @@ package com.example.mserrano.security.test.config;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
 public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
 
